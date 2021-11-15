9928f704b717c555e07e99c2be4ad93df06619ca
Kishan Vadodaria <kishanvadodaria@Kishans-MacBook-Pro.local>
2021-11-15 19:36:35 +0000

    Adding practical 8 with gcd.c code & README.md markdown file

---
+++ practical08/README.md
@@ -0,0 +1,7 @@
+## This is practical 8
+1. **gcd.c** - Computing greatest common divisor(GCD) of a pair of integers using euclidian algorithm by iterative & recursive function
+
+#### To compile and run the C programs
+1. gcd.c
+- gcc gcd.c -o gcd_c
+- ./gcd_c
---
+++ practical08/gcd.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+
+//Declare and define functions
+//Computing gcd using iterative function
+int iterativeGCD(int a,int b){
+   int temp;
+   while(b != 0){
+      temp=b;
+      b=a%b;
+      a=temp;
+   }
+   return a;
+}
+
+//Computing gcd using recursive function
+int recursiveGCD(int a, int b){
+   if(b==0)
+      return a;
+   else
+      return(recursiveGCD(b, a%b));
+}
+
+int main(void){
+   //declare variables
+   int a,b,error;
+
+   printf("Please inpute two positive integers\n");
+   error = scanf("%d %d",&a,&b); //get two positive integers from user
+   //checking for any error if found than terminate with error message
+   if(error != 2){
+      printf("Please try again\n");
+      return 1;
+   }
+   
+   //checking for negative numbers and if found then terminate with error message
+   if(a<=0 || b<=0){
+      printf("These numbers are not positive!\n");
+      return 1;
+   }
+
+   //Print and calling gcd functions
+   printf("Iterative gcd(%d,%d)=%d\n", a, b, iterativeGCD(a,b));
+   printf("Recursive gcd(%d,%d)=%d\n", a, b, recursiveGCD(a,b));
+
+   return 0;
+}
